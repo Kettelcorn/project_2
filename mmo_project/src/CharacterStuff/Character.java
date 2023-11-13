0694f5309815d26c03a5ba7b2667a9204fad522d
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2023-11-12 16:23:23 -0800

    Finised Character Class

--- mmo_project/src/CharacterStuff/Character.java
+++ mmo_project/src/CharacterStuff/Character.java
@@ -1,4 +1,51 @@
 package CharacterStuff;
 
 public class Character {
+    private String name;
+    private int height;
+    private int weight;
+    private double moralAlign;
+
+    public Character(String name, int height, int weight, double moralAlign) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.moralAlign = moralAlign;
+    }
+
+    /**
+     * Returns name of character
+     *
+     * @return name of character
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns height of character
+     *
+     * @return height of character
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Returns weight of character
+     *
+     * @return weight of character
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * Returns moral alignment of character
+     *
+     * @return moral alignment
+     */
+    public double getMoralAlign() {
+        return moralAlign;
+    }
 }
